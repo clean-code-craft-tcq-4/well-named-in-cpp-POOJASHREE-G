254aa543d04388752003ab352fcf3763bcd449ae
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-06-13 19:35:21 +0530

    Update ColorCoder_TestFunctions.cpp

--- ColorCoder_TestFunctions.cpp
+++ ColorCoder_TestFunctions.cpp
@@ -3,6 +3,19 @@
 #include "TelCoColorCoder.hpp"
 #include "Get_ColorPair_nums.hpp"
 
+TelCoColorCoder::ColorPair GetColorFromPairNumber(int pairNumber) {
+    int zeroBasedPairNumber = pairNumber - 1;
+    TelCoColorCoder::MajorColor majorColor = 
+        (TelCoColorCoder::MajorColor)(zeroBasedPairNumber / TelCoColorCoder::numberOfMinorColors);
+    TelCoColorCoder::MinorColor minorColor =
+        (TelCoColorCoder::MinorColor)(zeroBasedPairNumber % TelCoColorCoder::numberOfMinorColors);
+    return TelCoColorCoder::ColorPair(majorColor, minorColor);
+}
+
+int GetPairNumberFromColor(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor) {
+    return major * TelCoColorCoder::numberOfMinorColors + minor + 1;
+}
+
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor)
@@ -22,17 +35,4 @@ void testPairToNumber(
     int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
-}
-
-TelCoColorCoder::ColorPair GetColorFromPairNumber(int pairNumber) {
-    int zeroBasedPairNumber = pairNumber - 1;
-    TelCoColorCoder::MajorColor majorColor = 
-        (TelCoColorCoder::MajorColor)(zeroBasedPairNumber / TelCoColorCoder::numberOfMinorColors);
-    TelCoColorCoder::MinorColor minorColor =
-        (TelCoColorCoder::MinorColor)(zeroBasedPairNumber % TelCoColorCoder::numberOfMinorColors);
-    return TelCoColorCoder::ColorPair(majorColor, minorColor);
-}
-
-int GetPairNumberFromColor(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor) {
-    return major * TelCoColorCoder::numberOfMinorColors + minor + 1;
 }
