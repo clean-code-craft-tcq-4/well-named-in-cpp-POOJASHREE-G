58f8f7f113a41edf6bf727486d3d603d4aa0f250
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-06-14 16:16:33 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 #include "ColorCoder_TestFunctions.hpp"
 #include "Get_ColorPair_numbers.hpp"
 
+namespace TelCoColorCoder
+{
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+}
 
 int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
